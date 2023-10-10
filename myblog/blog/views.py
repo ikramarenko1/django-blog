0747f065ddbd1fad3678916ef55ebcee172b44dd
ikramarenko1 <il.kramarenko1@gmail.com>
2023-10-10 22:52:22 +0300

    update project

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Post
 
+
 # Create your views here.
-
-
 class PostView(View):
-    '''
+    """
     Вывод записей
-    '''
+    """
 
     def get(self, request):
         posts = Post.objects.all()
-        
+
         return render(request, 'blog/blog.html', {'posts': posts})
+
+
+class PostDetail(View):
+    """
+    Отдельная страница для записи
+    """
+
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+
+        return render(request, 'blog/blog_post.html', {'post': post})
