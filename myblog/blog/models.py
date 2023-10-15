f5464f5934cd66e11eb374b7f5fc33e6136848c5
ikramarenko1 <il.kramarenko1@gmail.com>
2023-10-15 22:56:03 +0300

    add comment model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -14,3 +14,17 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'Запись'
         verbose_name_plural = 'Записи'
+        
+
+class Comment(models.Model):
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=50)
+    content = models.TextField('Текст комментария', max_length=1000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)  # Когда удаляется объект публикации, все комментарии связаны с публикацией тоже удаляются
+    
+    def __str__(self) -> str:
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
